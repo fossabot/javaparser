5c3a3bea814ca6524306aeaedd8f8b405455ca92
Roger Howell <howelrtc@aston.ac.uk>
2020-04-26 19:45:11 +0100

    progress towards simpler test case

--- javaparser-core-testing/src/test/resources/com/github/javaparser/issue_samples/issue_2627/Ops_minimal.java
+++ javaparser-core-testing/src/test/resources/com/github/javaparser/issue_samples/issue_2627/Ops_minimal.java
@@ -1,187 +1,188 @@
 public final class Ops {
 
     /*
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-      Test test test test test test test test test test
-      Test test test test test test test test test test
-      Test test test test test test test test test test
-      Test test test test test test test test test test
-      Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * 
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * 
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * 
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * 
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * 
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * 
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * 
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * 
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * 
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
      */
 
     /*
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
-     Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     *
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * 
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * 
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * 
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * 
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
+     * Test test test test test test test test test test
      */
 
+    public void test() {}
 }
