87b6030d963db3eacbd3a27d3a81a5bb6c166d7c
Roger Howell <howelrtc@aston.ac.uk>
2020-04-26 14:04:18 +0100

    start of trying to extract out minimal test case

--- javaparser-core-testing/src/test/java/com/github/javaparser/issues/Issue2627Test.java
+++ javaparser-core-testing/src/test/java/com/github/javaparser/issues/Issue2627Test.java
@@ -21,7 +21,7 @@ public class Issue2627Test {
     }
 
     @Test
-    public void method_batchToSpace() throws IOException {
+    public void method_original_batchToSpace() throws IOException {
         CompilationUnit cu = StaticJavaParser.parseResource("com/github/javaparser/issue_samples/issue_2627/Ops.java");
 
         Optional<MethodDeclaration> node = getFirstMethodDeclarationByName(cu, "batchToSpace");
@@ -37,7 +37,7 @@ public class Issue2627Test {
     }
 
     @Test
-    public void method_batchToSpaceNd() throws IOException {
+    public void method_original_batchToSpaceNd() throws IOException {
         CompilationUnit cu = StaticJavaParser.parseResource("com/github/javaparser/issue_samples/issue_2627/Ops.java");
 
         Optional<MethodDeclaration> node = getFirstMethodDeclarationByName(cu, "batchToSpaceNd");
@@ -53,8 +53,56 @@ public class Issue2627Test {
     }
 
     @Test
-    public void method_placeHolder() throws IOException {
+    public void method_original_placeHolder() throws IOException {
         CompilationUnit cu = StaticJavaParser.parseResource("com/github/javaparser/issue_samples/issue_2627/Ops.java");
+
+        Optional<MethodDeclaration> node = getFirstMethodDeclarationByName(cu, "placeholder");
+
+        assertTrue(node.isPresent());
+        Range range = node.get().getRange().get();
+
+        System.out.println("range = " + range);
+
+        assertEquals(3686, range.begin.line);
+        assertEquals(3689, range.end.line);
+
+    }
+
+    @Test
+    public void method_minimal_batchToSpace() throws IOException {
+        CompilationUnit cu = StaticJavaParser.parseResource("com/github/javaparser/issue_samples/issue_2627/Ops_minimal.java");
+
+        Optional<MethodDeclaration> node = getFirstMethodDeclarationByName(cu, "batchToSpace");
+
+        assertTrue(node.isPresent());
+        Range range = node.get().getRange().get();
+
+        System.out.println("range = " + range);
+
+        assertEquals(796, range.begin.line);
+        assertEquals(799, range.end.line);
+
+    }
+
+    @Test
+    public void method_minimal_batchToSpaceNd() throws IOException {
+        CompilationUnit cu = StaticJavaParser.parseResource("com/github/javaparser/issue_samples/issue_2627/Ops_minimal.java");
+
+        Optional<MethodDeclaration> node = getFirstMethodDeclarationByName(cu, "batchToSpaceNd");
+
+        assertTrue(node.isPresent());
+        Range range = node.get().getRange().get();
+
+        System.out.println("range = " + range);
+
+        assertEquals(911, range.begin.line);
+        assertEquals(914, range.end.line);
+
+    }
+
+    @Test
+    public void method_minimal_placeHolder() throws IOException {
+        CompilationUnit cu = StaticJavaParser.parseResource("com/github/javaparser/issue_samples/issue_2627/Ops_minimal.java");
 
         Optional<MethodDeclaration> node = getFirstMethodDeclarationByName(cu, "placeholder");
 
