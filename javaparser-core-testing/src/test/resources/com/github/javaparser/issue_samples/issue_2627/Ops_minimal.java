69d9429339814dc1c180502ee86c43a787e3ab07
Roger Howell <howelrtc@aston.ac.uk>
2020-04-26 18:34:56 +0100

    progress towards simpler test case

--- javaparser-core-testing/src/test/resources/com/github/javaparser/issue_samples/issue_2627/Ops_minimal.java
+++ javaparser-core-testing/src/test/resources/com/github/javaparser/issue_samples/issue_2627/Ops_minimal.java
@@ -1,26 +1,18 @@
-package org.tensorflow.op;
-
 public final class Ops {
 
-  private Ops(Scope scope) {
-    this.scope = scope;
-    nn = new NnOps(scope);
-    summary = new SummaryOps(scope);
-    image = new ImageOps(scope);
-    data = new DataOps(scope);
-    io = new IoOps(scope);
-    dtypes = new DtypesOps(scope);
-    linalg = new LinalgOps(scope);
-    random = new RandomOps(scope);
-    strings = new StringsOps(scope);
-    sparse = new SparseOps(scope);
-    bitwise = new BitwiseOps(scope);
-    math = new MathOps(scope);
-    audio = new AudioOps(scope);
-    signal = new SignalOps(scope);
-    train = new TrainOps(scope);
-    quantization = new QuantizationOps(scope);
-  }
+  public void test0() {}
+  public void test1() {}
+  public void test2() {}
+  public void test3() {}
+  public void test4() {}
+  public void test5() {}
+  public void test6() {}
+  public void test7() {}
+  public void test8() {}
+  public void test9() {}
+  public void test10() {}
+  public void test11() {}
+  public void test12() {}
 
 
     /**
@@ -60,9 +52,13 @@ public final class Ops {
      * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
      * followed by cropping along the height and width dimensions.
      *
+     *
+     *
      * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
      * followed by cropping along the height and width dimensions.
      *
+     *
+     * 
      * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
      * followed by cropping along the height and width dimensions.
      *
@@ -70,116 +66,125 @@ public final class Ops {
      * followed by cropping along the height and width dimensions.
      *
      */
-    public int batchToSpace() {}
 
     /**
-     *
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
      * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
      * followed by cropping along the height and width dimensions.
      */
-    public void batchToSpaceNd() { }
 
+    /**
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     * P TAG
+     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
+     * the height and width dimensions,
+     * followed by cropping along the height and width dimensions.
+     */
 
-  /**
-   * ABC
-   * PARAGRAPH TAG
-   * ABC
-   * PARAGRAPH TAG
-   * ABC
-   * PARAGRAPH TAG
-   * ABC
-   * PARAGRAPH TAG
-   * ABC
-   * PARAGRAPH TAG
-   * ABC
-   */
-  public void test() {
-
-  }
 }
