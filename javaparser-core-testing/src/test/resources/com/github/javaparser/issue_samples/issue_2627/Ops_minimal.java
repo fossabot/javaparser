1d3945d89e3e90f3283f7776a267ec8634e55ce8
Roger Howell <howelrtc@aston.ac.uk>
2020-04-26 18:57:53 +0100

    progress towards simpler test case

--- javaparser-core-testing/src/test/resources/com/github/javaparser/issue_samples/issue_2627/Ops_minimal.java
+++ javaparser-core-testing/src/test/resources/com/github/javaparser/issue_samples/issue_2627/Ops_minimal.java
@@ -1,190 +1,253 @@
 public final class Ops {
 
-  public void test0() {}
-  public void test1() {}
-  public void test2() {}
-  public void test3() {}
-  public void test4() {}
-  public void test5() {}
-  public void test6() {}
-  public void test7() {}
-  public void test8() {}
-  public void test9() {}
-  public void test10() {}
-  public void test11() {}
-  public void test12() {}
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
+    // Test
 
 
-    /**
-     * ABC
-     * PARAGRAPH TAG
-     * ABC
-     * PARAGRAPH TAG
-     * ABC
-     * PARAGRAPH TAG
-     * ABC
-     * PARAGRAPH TAG
-     * ABC
-     * PARAGRAPH TAG
-     * ABC
-     * PARAGRAPH TAG
-     * ABC
-     * PARAGRAPH TAG
-     * ABC
-     * PARAGRAPH TAG
-     * ABC
-     * PARAGRAPH TAG
-     * ABC
-     * PARAGRAPH TAG
-     * ABC
-     * PARAGRAPH TAG
-     * ABC
-     * PARAGRAPH TAG
-     * ABC
-     * PARAGRAPH TAG
-     * ABC
-     * PARAGRAPH TAG
-     * ABC
-     * PARAGRAPH TAG
-     */
+    // test
+    // test
+    // test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
 
-    /**
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     *
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     *
-     * 
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     *
-     */
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
 
-    /**
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     */
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
 
-    /**
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
-     * P TAG
-     * this op outputs a copy of the input tensor where values from the batch dimension are moved in spatial blocks to
-     * the height and width dimensions,
-     * followed by cropping along the height and width dimensions.
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    /*
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
+    // test test test test test test test test test
      */
 
 }
