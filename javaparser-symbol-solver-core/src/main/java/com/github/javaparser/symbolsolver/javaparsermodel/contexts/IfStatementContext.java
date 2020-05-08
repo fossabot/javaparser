a5c12471b260ba0f97b396b5addb4bbfc8767fc8
Roger Howell <howelrtc@aston.ac.uk>
2020-05-08 15:58:43 +0100

    progress

--- javaparser-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/javaparsermodel/contexts/IfStatementContext.java
+++ javaparser-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/javaparsermodel/contexts/IfStatementContext.java
@@ -3,6 +3,10 @@ package com.github.javaparser.symbolsolver.javaparsermodel.contexts;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.expr.PatternExpr;
 import com.github.javaparser.ast.stmt.IfStmt;
+import com.github.javaparser.resolution.declarations.ResolvedValueDeclaration;
+import com.github.javaparser.symbolsolver.core.resolution.Context;
+import com.github.javaparser.symbolsolver.javaparsermodel.declarations.JavaParserSymbolDeclaration;
+import com.github.javaparser.symbolsolver.model.resolution.SymbolReference;
 import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
 
 import java.util.Collections;
@@ -15,6 +19,24 @@ public class IfStatementContext extends AbstractJavaParserContext<IfStmt> {
         super(wrappedNode, typeSolver);
     }
 
+
+    public SymbolReference<? extends ResolvedValueDeclaration> solveSymbol(String name) {
+        // If this IfStmt has the variable in this, then consider it solved...
+        List<PatternExpr> patternExprDirectlyInThisCondition = wrappedNode.getCondition().findAll(PatternExpr.class);
+        for (PatternExpr patternExpr : patternExprDirectlyInThisCondition) {
+            if (patternExpr.getName().getIdentifier().equals(name)) {
+                return SymbolReference.solved(JavaParserSymbolDeclaration.patternVar(patternExpr, typeSolver));
+            }
+        }
+
+        // Don't search in the "parent" if/elseif/else bits...
+        Context parentContext = getParent();
+        while (nodeContextIsNestedIf(parentContext) || nodeContextIsImmediateChildElse(parentContext)) {
+            parentContext = parentContext.getParent();
+        }
+
+        return getParent().solveSymbol(name);
+    }
 
     public List<PatternExpr> patternExprExposedToChild(Node child) {
         // If the given node is not within the "then" section, any PatternExpr variable is not within scope.
@@ -34,4 +56,60 @@ public class IfStatementContext extends AbstractJavaParserContext<IfStmt> {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * <pre>{@code
+     * if() {
+     *     // Does not match here (doesn't need to, as stuff inside of the if() is likely in context..)
+     * } else if() {
+     *     // Matches here
+     * } else {
+     *     // Matches here
+     * }
+     * }</pre>
+     *
+     * @return true, If this is an if inside of an if...
+     */
+    private boolean nodeContextIsNestedIf(Context parentContext) {
+        return parentContext instanceof AbstractJavaParserContext
+                && ((AbstractJavaParserContext<?>) this).getWrappedNode() instanceof IfStmt
+                && ((AbstractJavaParserContext<?>) parentContext).getWrappedNode() instanceof IfStmt;
+    }
+
+    /**
+     * <pre>{@code
+     * if() {
+     *     // Does not match here (doesn't need to, as stuff inside of the if() is likely in context..)
+     * } else {
+     *     // Does not match here, as the else block is a field inside of an ifstmt as opposed to child
+     * }
+     * }</pre>
+     *
+     * @return true, If this is an else inside of an if...
+     */
+    private boolean nodeContextIsImmediateChildElse(Context parentContext) {
+        if (!(parentContext instanceof AbstractJavaParserContext)) {
+            return false;
+        }
+        if (!(this instanceof AbstractJavaParserContext)) {
+            return false;
+        }
+
+        AbstractJavaParserContext<?> abstractContext = (AbstractJavaParserContext<?>) this;
+        AbstractJavaParserContext<?> abstractParentContext = (AbstractJavaParserContext<?>) parentContext;
+
+        Node wrappedNode = abstractContext.getWrappedNode();
+        Node wrappedParentNode = abstractParentContext.getWrappedNode();
+
+        if (wrappedParentNode instanceof IfStmt) {
+            IfStmt parentIfStmt = (IfStmt) wrappedParentNode;
+            if (parentIfStmt.getElseStmt().isPresent()) {
+                boolean currentNodeIsAnElseBlock = parentIfStmt.getElseStmt().get() == wrappedNode;
+                if (currentNodeIsAnElseBlock) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 }
